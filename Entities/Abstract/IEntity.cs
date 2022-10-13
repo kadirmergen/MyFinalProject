9c24e4fd1f18ad7645319d968c3b2df91db30de4
kadirmergen <mergenkdr@gmail.com>
2022-10-13 15:14:15 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implement eden class bir veritabanı tablosudur.Bu bizim kendi yorumumuz. 
+    
+    public interface IEntity
+    {
+    }
+}
