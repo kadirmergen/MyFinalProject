733293c85d5fda82d6d376cb9ef96c50b96383be
kadirmergen <mergenkdr@gmail.com>
2023-03-19 18:38:57 +0300

    19.03

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,6 +11,31 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            
+
+            ProductManager productManager2 = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager2.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName+" / "+product.CategoryName);
+            }
+
+            //ProductTest();
+            //IoC
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
             foreach (var product in productManager.GetAllByCategoryId(2))
@@ -18,12 +43,10 @@ namespace ConsoleUI
                 Console.WriteLine(product.ProductName);
             }
             Console.WriteLine("-----------");
-            foreach (var product2 in productManager.GetByUnitPrice(20,80))
+            foreach (var product2 in productManager.GetByUnitPrice(20, 80))
             {
                 Console.WriteLine(product2.ProductName);
             }
-
-            
         }
     }
 }
