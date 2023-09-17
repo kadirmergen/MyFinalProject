fefa44e1d1e136fe7f3bcb43607ef479a017d1b9
kadirmergen <mergenkdr@gmail.com>
2023-09-17 13:01:27 +0300

    17.09

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -8,11 +10,19 @@ namespace Business.Constants
     public static class Messages
     {
         public static string ProductAdded = "Ürün eklendi.";
-        public static string ProductNameInvalid= "Ürün ismi geçersiz.";
-        public static string MaintenanceTime= "Sistem bakımda...";
-        public static string ProductsListed="Ürünler listelendi.";
-        public static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir";
-        public static string ProductNameAlreadyExists="Böyle bir ürün ismi zaten var";
-        public static string CategoryLimitExcedeed="Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        public static string MaintenanceTime = "Sistem bakımda...";
+        public static string ProductsListed = "Ürünler listelendi.";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExists = "Böyle bir ürün ismi zaten var";
+        public static string CategoryLimitExcedeed = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string AuthorizationDenied = "Maalesef yetkiniz yok.";
+        public static string UserRegistered = "Kayıt oldu.";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string PasswordError = "Parola hatalı.";
+        public static string SuccessfulLogin = "Giriş başarılı.";
+        public static string UserAlreadyExists = "Kullanıcı mevcut";
+        public static string AccessTokenCreated = "Token oluşturuldu.";
+        public static string ClaimsListed = "Kullanıcı yetkileri listelelendi.";
     }
 }
